d3d37c1a502bbd99c8cbb654f3efb32836b8e5ac
JianMing <ljmwatermelon@gmail.com>
2024-09-13 13:37:57 +0800

    Initial commit

---
+++ Assets/Script/sceneTransition.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneTransition : MonoBehaviour
+{
+
+
+    public void SwitchScenes(string sceneName){
+        SceneManager.LoadScene(sceneName);
+
+    }
+
+}
+
